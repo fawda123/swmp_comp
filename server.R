6aa1cd6072472f4e645db45b03a6df6549e9add2
fawda123 <mbafs2012@gmail.com>
2015-01-04 20:17:03 -0600

    copy of jcheng leaflet example, time to break the app

--- server.R
+++ server.R
@@ -1,20 +1,29 @@
-# packages to use
 library(leaflet)
-library(ShinyDash)
-library(RColorBrewer)
-library(scales)
-library(lattice)
+library(ggplot2)
+library(maps)
 library(plyr)
-library(data.table)
-require(maps)
 
-# data to use
-load(file = 'data/all_dat.RData')
-
+data(uspop2000)
+data(all_dat)
 source('R/funcs.R')
 
-# Define server logic required to generate and plot data
+# From a future version of Shiny
+bindEvent <- function(eventExpr, callback, env=parent.frame(), quoted=FALSE) {
+  eventFunc <- exprToFunction(eventExpr, env, quoted)
+  
+  initialized <- FALSE
+  invisible(observe({
+    eventVal <- eventFunc()
+    if (!initialized)
+      initialized <<- TRUE
+    else
+      isolate(callback())
+  }))
+}
+
 shinyServer(function(input, output, session) {
+  
+  makeReactiveBinding('selectedstation')
   
   ## get data given data_type and var
   dat <- reactive({
@@ -23,19 +32,19 @@ shinyServer(function(input, output, session) {
     var <- strsplit(var, ': ')[[1]][2]
     dat <- all_dat[[var]]
     return(dat)
-
+    
   })
   
   output$years <- renderUI({
-  
+    
     yrs <- as.numeric(as.character(unique(dat()$year)))
-   
+    
     sliderInput("years", label = '',  
-      min = min(yrs), max = max(yrs), 
-      value = range(yrs),
-      format = '####'
+                min = min(yrs), max = max(yrs), 
+                value = range(yrs),
+                format = '####'
     )
-   
+    
   })
   
   to_plo <- reactive({
@@ -51,43 +60,153 @@ shinyServer(function(input, output, session) {
     
   })
   
-  map <- createLeafletMap(session, "map")
+  # Define some reactives for accessing the data
   
-  # A reactive expression that returns the set of zips that are
-  # in bounds right now
-  statsInBounds <- reactive({
+  # Retrieve the name of the column that contains the selected year's
+  # population
+  popCol <- reactive({
+    paste('Pop', input$year, sep='')
+  })
+  
+  popSeries <- function(city) {
+    c(
+      sum(city$Pop2000),
+      sum(city$Pop2001),
+      sum(city$Pop2002),
+      sum(city$Pop2003),
+      sum(city$Pop2004),
+      sum(city$Pop2005),
+      sum(city$Pop2006),
+      sum(city$Pop2007),
+      sum(city$Pop2008),
+      sum(city$Pop2009),
+      sum(city$Pop2010)
+    )
+  }
+  
+  # The cities that are within the visible bounds of the map
+  citiesInBounds <- reactive({
     if (is.null(input$map_bounds))
-      return(to_plo()[FALSE,])
+      return(uspop2000[FALSE,])
     bounds <- input$map_bounds
     latRng <- range(bounds$north, bounds$south)
     lngRng <- range(bounds$east, bounds$west)
-
-    subset(to_plo(),
-    Latitude >= latRng[1] & Latitude <= latRng[2] &
-    Longitude >= lngRng[1] & Longitude <= lngRng[2])
+    
+    browser()
+    
+    subset(uspop2000,
+           Lat >= latRng[1] & Lat <= latRng[2] &
+             Long >= lngRng[1] & Long <= lngRng[2])
   })
-
-
-  session$onFlushed(once=TRUE, function() {
-    
-    paintObs <- observe({
   
-      # Clear existing circles before drawing
-      map$clearShapes()
-      plo <- statsInBounds()
-      browser()
-      map$addCircle(plo$Longitude, plo$Latitude)
+  # The top N cities (by population) that are within the visible bounds
+  # of the map
+  topCitiesInBounds <- reactive({
+    cities <- citiesInBounds()
+    cities <- head(cities[order(cities[[popCol()]], decreasing=TRUE),],
+                   as.numeric(input$maxCities))
+  })
+  
+  # Create the map; this is not the "real" map, but rather a proxy
+  # object that lets us control the leaflet map on the page.
+  map <- createLeafletMap(session, 'map')
+  
+  observe({
+    if (is.null(input$map_click))
+      return()
+    selectedstation <<- NULL
+  })
+  
+  radiusFactor <- 1000
+  observe({
+    map$clearShapes()
+    cities <- topCitiesInBounds()
     
-  #   m <- m %>% setView(lng = -105.34, lat = 41.52, zoom = 3)
+    if (nrow(cities) == 0)
+      return()
     
-  #   m
-
+    map$addCircle(
+      cities$Lat,
+      cities$Long,
+      sqrt(cities[[popCol()]]) * radiusFactor / max(5, input$map_zoom)^2,
+      row.names(cities),
+      list(
+        weight=1.2,
+        fill=TRUE,
+        color='#4A9'
+      )
+    )
+  })
+  
+  observe({
+    event <- input$map_shape_click
+    if (is.null(event))
+      return()
+    map$clearPopups()
+    
+    isolate({
+      cities <- topCitiesInBounds()
+      city <- cities[row.names(cities) == event$id,]
+      selectedstation <<- city
+      content <- as.character(tagList(
+        tags$strong(paste(city$City, city$State)),
+        tags$br(),
+        sprintf("Estimated population, %s:", input$year),
+        tags$br(),
+        prettyNum(city[[popCol()]], big.mark=',')
+      ))
+      map$showPopup(event$lat, event$lng, content, event$id)
     })
-  
-  # TIL this is necessary in order to prevent the observer from
-  # attempting to write to the websocket after the session is gone.
-  session$onSessionEnded(paintObs$suspend)
   })
-
-})
   
+  output$desc <- reactive({
+    if (is.null(input$map_bounds))
+      return(list())
+    list(
+      lat = mean(c(input$map_bounds$north, input$map_bounds$south)),
+      lng = mean(c(input$map_bounds$east, input$map_bounds$west)),
+      zoom = input$map_zoom,
+      shownCities = nrow(topCitiesInBounds()),
+      totalCities = nrow(citiesInBounds())
+    )
+  })
+  
+  output$data <- renderTable({
+    if (nrow(topCitiesInBounds()) == 0)
+      return(NULL)
+    
+    data.frame(
+      City = paste(topCitiesInBounds()$City, topCitiesInBounds()$State),
+      Population = topCitiesInBounds()[[popCol()]])
+  }, include.rownames = FALSE)
+  
+  # plot label here
+  output$cityTimeSeriesLabel <- renderText({
+    if (is.null(selectedstation)) {
+      'Total population of visible cities'
+    } else {
+      paste('Population of ',
+            selectedstation$City,
+            ', ',
+            selectedstation$State,
+            sep='')
+    }
+  })
+  
+  # plot here
+  output$cityTimeSeries <- renderPlot({
+    cities <- NULL
+    if (!is.null(selectedstation))
+      cities <- selectedstation
+    else
+      cities <- topCitiesInBounds()
+    
+    popData <- popSeries(cities) / 1000
+    df <- data.frame(year = c(2000:2010), pop = popData)
+    p <- ggplot(df, aes(x = year, y = pop)) + geom_line()
+    p <- p + ylim(c(0, max(popData)))
+    p <- p + ylab('Population (thousands)')
+    p <- p + scale_x_continuous(breaks = seq(2000, 2010, 2))
+    print(p)
+  })
+})
--- ui.R
+++ ui.R
@@ -1,83 +1,98 @@
 library(leaflet)
-library(shiny)
 library(ShinyDash)
-shinyUI(fluidPage(
-  
-  theme = 'styles.css',
-  
-  # Application title
-  h2("Trends in SWMP parameters across stations"),
-  
-  h4('Created by Marcus W. Beck,', a('beck.marcus@epa.gov', href = 'mailto:beck.marcus@epa.gov')),
-  
-  p('This interactive widget provides graphical summaries of trends over time in SWMP parameters across all stations.'),
 
-  # buttons on top
+shinyUI(fluidPage(
+  tags$head(tags$link(rel='stylesheet', type='text/css', href='styles.css')),
+  leafletMap(
+    "map", "100%", 400,
+    initialTileLayer = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
+    initialTileLayerAttribution = HTML('Maps by <a href="http://www.mapbox.com/">Mapbox</a>'),
+    options=list(
+      center = c(37.45, -93.85),
+      zoom = 4,
+      maxBounds = list(list(17, -180), list(59, 180))
+    )
+  ),
+  
   fluidRow(
-    
-    column(4, 
-      h3('Select parameter'),
-     
-      selectInput(inputId = "var", label = '',  
-        selected = 'nut: po4f',
-        choices = list(
-          'wq: Temperature (C)' = 'wq: temp',
-          'wq: Specific conductivity (mS/cm)' = 'wq: spcond',
-          'wq: Salinity (psu)' = 'wq: sal',
-          'wq: Dissolved oxyxgen (%)' = 'wq: do_pct',
-          'wq: Dissolved oxygen (mg/L)' = 'wq: do_mgl',
-          'wq: Depth (m)' = 'wq: depth',
-          'wq: Referenced depth (m)' = 'wq: level',
-          'wq: pH' = 'wq: ph',
-          'wq: Turbidity (NTU)' = 'wq: turb',
-          'wq: Chl fluorescence (ug/L)' = 'wq: chlfluor',
-          'met: Air temperature (C)' = 'met: atemp',
-          'met: Relative humidity (%)' = 'met: rh',
-          'met: Barometric pressure (mb)' = 'met: bp',
-          'met: Wind speed (m/s)' = 'met: wspd',
-          'met: Max wind speed (m/s)' = 'met: maxwspd',
-          'met: Wind direction (degrees)' = 'met: wdir',
-          'met: Wind direction (sd, degrees)' = 'met: sdwdir',
-          'met: Total PAR (mmol/m2)' = 'met: totpar',
-          'met: Total precipitation (mm)' = 'met: totprcp',
-          'met: Cumulative precipitation (mm)' = 'met: cumprcp',
-          'met: Total solar radiation (watts/m2)' = 'met: totsorad',
-          'nut: Orthophosphate (mg/L)' = 'nut: po4f', 
-          'nut: Ammonium (mg/L)' = 'nut: nh4f',
-          'nut: Nitrite (mg/L)' = 'nut: no2f', 
-          'nut: Nitrate (mg/L)' = 'nut: no3f',
-          'nut: Nitrite + Nitrate (mg/L)' = 'nut: no23f', 
-          'nut: Chlorophyll-a (ug/L)' = 'nut: chla_n'
-        )
-      )
       
+    column(4, 
+           h3('Select parameter'),
+           
+           selectInput(inputId = "var", label = '',  
+                       selected = 'nut: po4f',
+                       choices = list(
+                         'wq: Temperature (C)' = 'wq: temp',
+                         'wq: Specific conductivity (mS/cm)' = 'wq: spcond',
+                         'wq: Salinity (psu)' = 'wq: sal',
+                         'wq: Dissolved oxyxgen (%)' = 'wq: do_pct',
+                         'wq: Dissolved oxygen (mg/L)' = 'wq: do_mgl',
+                         'wq: Depth (m)' = 'wq: depth',
+                         'wq: Referenced depth (m)' = 'wq: level',
+                         'wq: pH' = 'wq: ph',
+                         'wq: Turbidity (NTU)' = 'wq: turb',
+                         'wq: Chl fluorescence (ug/L)' = 'wq: chlfluor',
+                         'met: Air temperature (C)' = 'met: atemp',
+                         'met: Relative humidity (%)' = 'met: rh',
+                         'met: Barometric pressure (mb)' = 'met: bp',
+                         'met: Wind speed (m/s)' = 'met: wspd',
+                         'met: Max wind speed (m/s)' = 'met: maxwspd',
+                         'met: Wind direction (degrees)' = 'met: wdir',
+                         'met: Wind direction (sd, degrees)' = 'met: sdwdir',
+                         'met: Total PAR (mmol/m2)' = 'met: totpar',
+                         'met: Total precipitation (mm)' = 'met: totprcp',
+                         'met: Cumulative precipitation (mm)' = 'met: cumprcp',
+                         'met: Total solar radiation (watts/m2)' = 'met: totsorad',
+                         'nut: Orthophosphate (mg/L)' = 'nut: po4f', 
+                         'nut: Ammonium (mg/L)' = 'nut: nh4f',
+                         'nut: Nitrite (mg/L)' = 'nut: no2f', 
+                         'nut: Nitrate (mg/L)' = 'nut: no3f',
+                         'nut: Nitrite + Nitrate (mg/L)' = 'nut: no23f', 
+                         'nut: Chlorophyll-a (ug/L)' = 'nut: chla_n'
+                       )
+           )
+           
     ),
     
     column(4,
-      h3('Select date range'), 
-    
-      uiOutput("years")
-      
+           h3('Select date range'), 
+           
+           uiOutput("years")
+           
     )
-  
-),
+    
+  ),
   
   fluidRow(
-    
-    column(12, 
-
-    leafletMap("map", width="100%", height="60%",
-      initialTileLayer = "//{s}.tiles.mapbox.com/v3/jcheng.map-5ebohr46/{z}/{x}/{y}.png",
-      initialTileLayerAttribution = HTML('Maps by <a href="http://www.mapbox.com/">Mapbox</a>'),
-      options=list(
-        center = c(37.45, -93.85),
-        zoom = 5, maxZoom=9,
-        maxBounds = list(list(15.961329,-129.92981), list(52.908902,-56.80481)) # Show US only
-        )
-      )
-    
+    column(8, offset=3,
+           h2('Trends in estuarine water quality parameters using SWMP'),
+           htmlWidgetOutput(
+             outputId = 'desc',
+             HTML(paste(
+               'The map is centered at <span id="lat"></span>, <span id="lng"></span>',
+               'with a zoom level of <span id="zoom"></span>.<br/>',
+               'Top <span id="shownCities"></span> out of <span id="totalCities"></span> visible cities are displayed.'
+             ))
+           )
     )
-    
+  ),
+
+  hr(),
+  fluidRow(
+    column(3,
+           selectInput('year', 'Year', c(2000:2010), 2010),
+           selectInput('maxCities', 'Maximum cities to display', choices=c(
+             5, 25, 50, 100, 200, 500, 2000, 5000, 10000, All = 100000
+           ), selected = 100)
+    ),
+    column(4,
+           h4('Visible cities'),
+           tableOutput('data')
+    ),
+    column(5,
+           h4(id='cityTimeSeriesLabel', class='shiny-text-output'),
+           plotOutput('cityTimeSeries', width='100%', height='250px')
+    )
   )
 
 ))
