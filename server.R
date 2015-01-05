140ef55ba8b13df30c36fe8261b29c7d794f9dfe
fawda123 <mbafs2012@gmail.com>
2015-01-04 21:36:56 -0600

    very close

--- server.R
+++ server.R
@@ -2,6 +2,7 @@ library(leaflet)
 library(ggplot2)
 library(maps)
 library(plyr)
+library(data.table)
 
 data(uspop2000)
 data(all_dat)
@@ -23,8 +24,7 @@ bindEvent <- function(eventExpr, callback, env=parent.frame(), quoted=FALSE) {
 
 shinyServer(function(input, output, session) {
   
-  makeReactiveBinding('selectedstation')
-  
+  ## Define some reactives for accessing the data
   ## get data given data_type and var
   dat <- reactive({
     
@@ -59,154 +59,117 @@ shinyServer(function(input, output, session) {
     return(to_plo)
     
   })
-  
-  # Define some reactives for accessing the data
-  
-  # Retrieve the name of the column that contains the selected year's
-  # population
-  popCol <- reactive({
-    paste('Pop', input$year, sep='')
-  })
-  
-  popSeries <- function(city) {
-    c(
-      sum(city$Pop2000),
-      sum(city$Pop2001),
-      sum(city$Pop2002),
-      sum(city$Pop2003),
-      sum(city$Pop2004),
-      sum(city$Pop2005),
-      sum(city$Pop2006),
-      sum(city$Pop2007),
-      sum(city$Pop2008),
-      sum(city$Pop2009),
-      sum(city$Pop2010)
-    )
-  }
-  
-  # The cities that are within the visible bounds of the map
-  citiesInBounds <- reactive({
+    
+  # The stations that are within the visible bounds of the map
+  statsInBounds <- reactive({
     if (is.null(input$map_bounds))
       return(uspop2000[FALSE,])
     bounds <- input$map_bounds
     latRng <- range(bounds$north, bounds$south)
     lngRng <- range(bounds$east, bounds$west)
     
-    browser()
+#     browser()
     
-    subset(uspop2000,
-           Lat >= latRng[1] & Lat <= latRng[2] &
-             Long >= lngRng[1] & Long <= lngRng[2])
-  })
-  
-  # The top N cities (by population) that are within the visible bounds
-  # of the map
-  topCitiesInBounds <- reactive({
-    cities <- citiesInBounds()
-    cities <- head(cities[order(cities[[popCol()]], decreasing=TRUE),],
-                   as.numeric(input$maxCities))
+    subset(to_plo(),
+           Latitude >= latRng[1] & Latitude <= latRng[2] &
+             Longitude >= lngRng[1] & Longitude <= lngRng[2])
   })
   
   # Create the map; this is not the "real" map, but rather a proxy
   # object that lets us control the leaflet map on the page.
   map <- createLeafletMap(session, 'map')
   
+  makeReactiveBinding('selectedstation')
+
   observe({
     if (is.null(input$map_click))
       return()
     selectedstation <<- NULL
   })
   
-  radiusFactor <- 1000
   observe({
     map$clearShapes()
-    cities <- topCitiesInBounds()
+    stations <- statsInBounds()
     
-    if (nrow(cities) == 0)
+    if (nrow(stations) == 0)
       return()
     
-    map$addCircle(
-      cities$Lat,
-      cities$Long,
-      sqrt(cities[[popCol()]]) * radiusFactor / max(5, input$map_zoom)^2,
-      row.names(cities),
+#     browser()
+    
+    # colors
+    col_vec <- unique(stations[, c('lab', 'cols')])
+    col_vec <- colorFactor(palette = col_vec$cols, levels = col_vec$lab)
+    
+#     browser()
+
+    map$addCircleMarker(
+      stations$Latitude,
+      stations$Longitude,
+      stations$pval_rad, 
+      rownames(stations),
       list(
         weight=1.2,
         fill=TRUE,
-        color='#4A9'
+        color=col_vec(stations$lab)
       )
     )
+
   })
   
   observe({
-    event <- input$map_shape_click
+    event <- input$map_marker_click
     if (is.null(event))
       return()
     map$clearPopups()
     
+    stations <- statsInBounds()
+    
+#     browser()
     isolate({
-      cities <- topCitiesInBounds()
-      city <- cities[row.names(cities) == event$id,]
-      selectedstation <<- city
+      stat <- stations[row.names(stations) == event$id,]
+      selectedstation <<- stat
       content <- as.character(tagList(
-        tags$strong(paste(city$City, city$State)),
+        tags$strong(stat$stat),
         tags$br(),
-        sprintf("Estimated population, %s:", input$year),
-        tags$br(),
-        prettyNum(city[[popCol()]], big.mark=',')
+        stat$lab
       ))
       map$showPopup(event$lat, event$lng, content, event$id)
     })
   })
   
+  # for description of widget
   output$desc <- reactive({
     if (is.null(input$map_bounds))
       return(list())
     list(
-      lat = mean(c(input$map_bounds$north, input$map_bounds$south)),
-      lng = mean(c(input$map_bounds$east, input$map_bounds$west)),
-      zoom = input$map_zoom,
-      shownCities = nrow(topCitiesInBounds()),
-      totalCities = nrow(citiesInBounds())
+      lat = round(mean(c(input$map_bounds$north, input$map_bounds$south)), 2),
+      lng = round(mean(c(input$map_bounds$east, input$map_bounds$west)), 2),
+      zoom = input$map_zoom
     )
   })
   
-  output$data <- renderTable({
-    if (nrow(topCitiesInBounds()) == 0)
-      return(NULL)
-    
-    data.frame(
-      City = paste(topCitiesInBounds()$City, topCitiesInBounds()$State),
-      Population = topCitiesInBounds()[[popCol()]])
-  }, include.rownames = FALSE)
-  
-  # plot label here
-  output$cityTimeSeriesLabel <- renderText({
-    if (is.null(selectedstation)) {
-      'Total population of visible cities'
-    } else {
-      paste('Population of ',
-            selectedstation$City,
-            ', ',
-            selectedstation$State,
-            sep='')
-    }
-  })
+#   # plot label here
+#   output$cityTimeSeriesLabel <- renderText({
+#     if (is.null(selectedstation)) {
+#       'Total population of visible cities'
+#     } else {
+#       paste('Population of ',
+#             selectedstation$City,
+#             ', ',
+#             selectedstation$State,
+#             sep='')
+#     }
+#   })
   
   # plot here
-  output$cityTimeSeries <- renderPlot({
-    cities <- NULL
+  output$statid <- renderPlot({
     if (!is.null(selectedstation))
-      cities <- selectedstation
+      stat <- selectedstation
     else
-      cities <- topCitiesInBounds()
+      return()
     
-    popData <- popSeries(cities) / 1000
-    df <- data.frame(year = c(2000:2010), pop = popData)
-    p <- ggplot(df, aes(x = year, y = pop)) + geom_line()
-    p <- p + ylim(c(0, max(popData)))
-    p <- p + ylab('Population (thousands)')
-    p <- p + scale_x_continuous(breaks = seq(2000, 2010, 2))
+    p <- plot(rnorm(100))
     print(p)
   })
+
 })
